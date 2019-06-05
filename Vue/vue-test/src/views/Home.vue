4763ab7dac87f1212f35e55995f68b5748789b80
hefeng6500 <2443992009@qq.com>
2019-06-05 21:00:22 +0800

    测试需求 select-tree组件

--- Vue/vue-test/src/views/Home.vue
+++ Vue/vue-test/src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
     <h1>Home</h1>
+    <!-- <datePicker
+      :options="calendarArr"
+      class="calendar"
+      @handleClickDay="handleClickDay"
+      @handlePrevMonth="handlePrevMonth"
+      @handleNextMonth="handleNextMonth"
+    /> -->
   </div>
 </template>
 
 <script>
-  export default {
-    name: "home"
+import datePicker from "./components/date-picker";
+export default {
+  name: "date-picker",
+  data() {
+    return {
+      calendarArr: {
+        type: "combination",
+        headStyle: {
+          todayBtn: "right",
+          combination: "center",
+          checkBtn: "right"
+        },
+        viewStyle: {
+          day: "right"
+        },
+        calendarData: []
+      }
+    };
+  },
+  components: {
+    datePicker
+  },
+  methods: {
+    handleClickDay() {
+      console.log('handleClickDay')
+    },
+    handlePrevMonth() {},
+    handleNextMonth() {}
   }
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style>
 </style>
--- Vue/vue-test/src/views/orderManage.vue
+++ Vue/vue-test/src/views/orderManage.vue
@@ -1,15 +1,119 @@
 <template>
   <div>
     <h1>订单管理</h1>
+    {{ value }}
+    <treeselect
+      :multiple="true"
+      :options="options"
+      :value-consists-of="valueConsistsOf"
+      :normalizer="normalizer"
+      v-model="value"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'orderManage'
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+
+export default {
+  name: "orderManage",
+  components: { Treeselect },
+  data() {
+    return {
+      valueConsistsOf: "LEAF_PRIORITY",
+      normalizer(node) {
+        return {
+          id: node.key,
+          label: node.name,
+          children: node.subOptions
+        };
+      },
+      // define default value
+      value: null,
+      // define options
+      options: [
+        {
+          key: "company",
+          name: "Company 🏢",
+          subOptions: [
+            {
+              key: "team-i",
+              name: "Team I 👥",
+              subOptions: [
+                {
+                  key: "person-a",
+                  name: "Person A 👱"
+                },
+                {
+                  key: "person-b",
+                  name: "Person B 🧔"
+                }
+              ]
+            },
+            {
+              key: "team-ii",
+              name: "Team II 👥",
+              subOptions: [
+                {
+                  key: "person-c",
+                  name: "Person C 👳"
+                },
+                {
+                  key: "person-d",
+                  name: "Person D 👧"
+                }
+              ]
+            },
+            {
+              key: "person-e",
+              name: "Person E 👩"
+            }
+          ]
+        },
+        {
+          key: "company2",
+          name: "Company2 🏢",
+          subOptions: [
+            {
+              key: "team-i2",
+              name: "Team I2 👥",
+              subOptions: [
+                {
+                  key: "person-a2",
+                  name: "Person A2 👱"
+                },
+                {
+                  key: "person-b2",
+                  name: "Person B2 🧔"
+                }
+              ]
+            },
+            {
+              key: "team-ii2",
+              name: "Team II2 👥",
+              subOptions: [
+                {
+                  key: "person-c2",
+                  name: "Person C2 👳"
+                },
+                {
+                  key: "person-d2",
+                  name: "Person D2 👧"
+                }
+              ]
+            },
+            {
+              key: "person-e2",
+              name: "Person E2 👩"
+            }
+          ]
+        }
+      ]
+    };
   }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
