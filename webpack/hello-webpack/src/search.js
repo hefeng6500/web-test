5c3cda18b084c1c1d08a86377265884b80ebb2f0
hefeng6500 <2443992009@qq.com>
2020-03-03 21:04:01 +0800

    feat: 添加对 react jsx 语法值支持

--- webpack/hello-webpack/src/search.js
+++ webpack/hello-webpack/src/search.js
@@ -1,1 +1,21 @@
-document.write('search page')
+"use strict";
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>Search Text</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Search/>,
+  document.getElementById('root')
+)
--- webpack/hello-webpack/webpack.config.js
+++ webpack/hello-webpack/webpack.config.js
@@ -23,15 +23,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Hello World app',
-      minify: { // 压缩HTML文件
-        removeComments: true, // 移除HTML中的注释
-        collapseWhitespace: true, // 删除空白符与换行符
-        minifyCSS: true// 压缩内联css
-      },
-      filename: 'index.html',
-      template: 'index.html'
-    })
+
   ]
 }
