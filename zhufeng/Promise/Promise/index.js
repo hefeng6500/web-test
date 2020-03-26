f4bada6c010ff945a9e0a0533e391eb638a55366
hefeng6500 <2443992009@qq.com>
2020-03-26 22:03:34 +0800

    feat: 学习 Promise 的链式调用，跑通 promises-aplus-tests 测试用例

--- zhufeng/Promise/Promise/index.js
+++ zhufeng/Promise/Promise/index.js
@@ -33,26 +33,107 @@ class Promise {
       exector(resolve, reject)
     } catch (e) {
       console.log('try catch', e);
+      reject(e)
     }
   }
 
   then(onfulfilled, onrejected) {
-    if (this.status === RESOLVED) {
-      onfulfilled(this.value)
-    }
-    if (this.status === REJECTED) {
-      onrejected(this.reason)
-    }
+    onfulfilled = typeof onfulfilled === 'function' ? onfulfilled : v => v
+    onrejected = typeof onrejected === 'function' ? onrejected : e => { throw e }
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.status === RESOLVED) {
+        setTimeout(() => {
+          try {
+            let x = onfulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = onrejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
 
-    if(this.status === PENDING){
-      this.onResolvedCallbacks.push(()=>{
-        onfulfilled(this.value)
-      })
-      this.onRejectedCallbacks.push(()=>{
-        onrejected(this.reason)
-      })
-    }
+      }
+
+      if (this.status === PENDING) {
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onfulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onrejected(this.reason)
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+      }
+    })
+
+
+    return promise2
   }
 }
 
-module.exports = Promise
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+  }
+
+  let called
+  if ((typeof x === 'object' && x != null) || typeof x === 'function') {
+    try {
+      let then = x.then
+      if (typeof then === 'function') {
+        then.call(x, (y) => {
+          if (called) return
+          called = true
+          // 递归解析 promise 
+          resolvePromise(promise2, y, resolve, reject)
+        }, (r) => {
+          if (called) return
+          called = true
+          reject(r)
+        })
+      } else {
+        resolve(x)
+      }
+    } catch (e) {
+      if (called) return
+      called = true
+      reject(e)
+    }
+
+  } else {
+    resolve(x)
+  }
+}
+
+// module.exports = Promise
+
+Promise.defer = Promise.deferred = function () {
+  let dfd = {}
+  dfd.promise = new Promise((resolve,reject)=>{
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+}
+module.exports = Promise;
--- zhufeng/Promise/index.js
+++ zhufeng/Promise/index.js
@@ -1,21 +1,40 @@
 const Promise = require('./Promise/index.js')
 
 let promise = new Promise((resolve, reject) => {
-  
+
   // throw new Error('这是错误哦')
-  // resolve('data')
+  resolve('data')
   // reject('data')
 
-  setTimeout(() => {
-    reject('data')
-  }, 1000);
+  // setTimeout(() => {
+  //   reject('data')
+  // }, 1000);
 
 })
 
-promise.then(res => {
-  console.log("resolve", res);
+let p = promise.then(res => {
+  // console.log("resolve", res);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('hello')
+    }, 1000);
+  })
+})
 
+// p.then(res => {
+//   console.log("res", res);
+
+// }, err => {
+//   console.log("err", err);
+
+// }).then(()=>{
+
+// })
+p.then().then().then(data => { 
+  console.log(data);
+  
 }, err => {
-  console.log("reject", err);
+  console.log(err);
 
 })
+
