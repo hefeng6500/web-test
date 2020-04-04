b9554c5f46ee00992045888cf61d11168f46c077
hefeng6500 <2443992009@qq.com>
2020-04-04 20:09:07 +0800

    fix: 再次提交

--- zhufeng/Promise/index.js
+++ zhufeng/Promise/index.js
@@ -1,40 +1,33 @@
+
+
 const Promise = require('./Promise/index.js')
 
 let promise = new Promise((resolve, reject) => {
 
-  // throw new Error('这是错误哦')
+  /** 同步Promise */
+  // throw new Error('this is a error')
   resolve('data')
-  // reject('data')
+  // reject('err')
 
+  /** 异步 Promise */
   // setTimeout(() => {
-  //   reject('data')
-  // }, 1000);
+  //   throw 'err'
+  // }, 0);
+
 
 })
 
-let p = promise.then(res => {
-  // console.log("resolve", res);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject('hello')
-    }, 1000);
-  })
-})
+let promise2 = promise.then(res => {
+  console.log(res);
+  // return res
+  throw new Error('this is a error')
+  // return res
+  /** return Promise */
+  // return new Promise((resolve, reject)=>{
+  //   resolve(res)
+  // })
 
-// p.then(res => {
-//   console.log("res", res);
-
-// }, err => {
-//   console.log("err", err);
-
-// }).then(()=>{
-
-// })
-p.then().then().then(data => { 
-  console.log(data);
-  
 }, err => {
   console.log(err);
 
 })
-
